cacd2dd347b2c986e7a60f22b7a9989471c40ca3
Eduardo Gutkoski <eduardo.pedogutkoski@gmail.com>
2025-03-03 16:46:27 -0300

    Add subscriber invite clicks functionality and update routes

---
+++ src/routes/get-subscriber-invite-clicks-route.ts
@@ -0,0 +1,34 @@
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { getSubscriberInviteClicks } from '../functions/get-subscriber-invite-clicks'
+
+export const getSubscriberInviteClicksRoute: FastifyPluginAsyncZod =
+  async app => {
+    app.get(
+      '/subscribers/:subscriberId/ranking/clicks',
+      {
+        schema: {
+          summary: 'Get subscriber invite clicks',
+          tags: ['referral'],
+          params: z.object({
+            subscriberId: z.string().uuid(),
+          }),
+          response: {
+            200: z.object({
+              count: z.number(),
+            }),
+          },
+        },
+      },
+      async (request, reply) => {
+        const { subscriberId } = request.params
+
+        const { count } = await getSubscriberInviteClicks({
+          subscriberId,
+        })
+
+        return reply.status(200).send({ count })
+      }
+    )
+  }
